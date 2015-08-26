95c643628a4a69047409da66ec064c8575ab7a8d
Hayden (Technoguyfication) Andreyka <haydenandreyka@gmail.com>
2015-08-25 18:30:07 -0700

    Starting work on config verification

--- Baackup/Tools.cs
+++ Baackup/Tools.cs
@@ -8,6 +8,48 @@ namespace Baackup
 {
     public class Tools
     {
+        /// <summary>
+        /// Validates the configuration settings.
+        /// </summary>
+        /// <returns>Configuration Validity State</returns>
+        public bool ValidateConfig()
+        {
+            try
+            {
+                #region RCON
+
+                if (Program.usercon)
+                {
+                    if (string.IsNullOrEmpty(Program.rconpass))
+                        throw new Exception("RCON Password Blank");
+                    else
+                        if (string.IsNullOrEmpty(Program.rconhostname))
+                            { } // do something
+                    else
+                        if (string.IsNullOrEmpty(Program.rconport.ToString()))
+                        }
+
+                #endregion
+
+                #region Worlds Container
+
+                if (Program.worldscontaineractive)
+                    if (string.IsNullOrEmpty(Program.worldscontainerpath))
+                        throw new Exception("Worlds Container");
+
+                #endregion
+            }
+            catch (Exception e)
+            {
+                Tools.Log("Could not verify config: " + e.Message + ". Program will terminate.", "Fatal");
+                return false;
+            }
+
+            // Assuming no errors were thrown
+            return true;
+        }
+
+        #region Other Stuff
         public static void Print(string message, bool line = true)
         {
             if (line)
@@ -16,9 +58,9 @@ namespace Baackup
                 Console.Write(message);
         }
 
-        public static void Log(string text)
+        public static void Log(string text, string prefix = "INFO")
         {
-            Print("[Baackup - " + DateTime.Now.ToString("hh:mm:ss") + "] " + text);
+            Print("[Baackup - " + DateTime.Now.ToString("hh:mm:ss") + "] [" + prefix.ToUpper() + "] " + text);
         }
 
         public static void Pause(string message = "")
@@ -52,5 +94,7 @@ namespace Baackup
         {
             Program.backupid = Program.backupscustomidprefix + '_' + DateTime.Now.ToString("yyyy-mmm-dd.hh-mm-ss-ffff--backup");
         }
+
+        #endregion
     }
 }
